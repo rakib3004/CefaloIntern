54b61adeeb0f0d5a6ca9ca27770cd8a000084cd6
rakib3004 <bsse1129@iit.du.ac.bd>
2023-01-12 19:03:29 +0600

    run code with out express JS only using node js

---
+++ HttpCreateServer.js
@@ -0,0 +1,29 @@
+const http = require('http');
+
+const server = http.createServer((req, res)=>{
+
+    if(req.url=='/'){
+        res.write('Hello World');
+        res.end();
+    }
+
+    if(req.url=='/api/courses'){
+        res.write(JSON.stringify([1,2,3]));
+        res.end();
+
+    }  
+});
+
+server.listen(3030);
+
+
+console.log('This server is running on PORT 3000.....');
+
+console.log('Hello World');
+
+
+
+
+
+
+
