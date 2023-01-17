f25b34a5b4b9bb3f8e86aededfa04f7ebc10b691
rakib3004 <bsse1129@iit.du.ac.bd>
2023-01-17 12:04:12 +0600

    add new response send into the  clinet point

--- status-code.js
+++ status-code.js
@@ -3,3 +3,21 @@
 // redirects response 3xx
 // client error 4xx
 // server error 5xx
+const express = require('express');
+
+const app = express();
+
+app.get('/admin', (req,res)=>{
+
+
+
+    res.send("<h1>Welcome to Admin Pannel<h1/> </br> <h2>Rational Stories<h2/>");
+});
+
+const PORT = 3000;
+
+
+app.listen(PORT, ()=>{
+
+    console.log('listening on port 3000');
+})
