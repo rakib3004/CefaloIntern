df67c945f44867935e86eb03cd112ae0417d0379
rakib3004 <bsse1129@iit.du.ac.bd>
2023-02-06 16:56:03 +0600

    add object prototypes code

--- touch-first-step/object-prototypes.js
+++ touch-first-step/object-prototypes.js
@@ -1,11 +1,18 @@
-function Person(first, last, age, eyecolor) {
-    this.firstName = first;
-    this.lastName = last;
-    this.age = age;
-    this.eyeColor = eyecolor;
-  }
-  
-  const myFather = new Person("John", "Doe", 50, "blue");
-  const myMother = new Person("Sally", "Rally", 48, "green");
 
+
+function Student(roll,name,company,salary){
+    this.rollNo = roll;
+    this.fullName=name;
+    this.companyName=company;
+    this.internSalary=salary;
+}
+
+
+
+const rakib = new Student(1129,"Md. Rakib Trofder", "Cefalo",15000);
+const siam = new Student(1104,"Md. Siam", "Stream Tech",10000);
+const jafar = new Student(1109,"Jafar Mahin", "Orion",20000);
+const kazi = new Student(1111,"Kazi Muktadir Ahmed", "Brain Station 23",10000);
+const galib = new Student(1130,"Mushfiqur Galib", "Kaz Software",10000);
+const mushfiq = new Student(1110,"Mushfiqur Rahman Chowdhury", "Samsung",25000);
 
