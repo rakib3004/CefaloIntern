8b8f0c26c3f7dd652ee61cff5c8ece44f40d43f8
rakib3004 <bsse1129@iit.du.ac.bd>
2023-02-07 11:17:00 +0600

    add new arrow-regular functions file

---
+++ touch-first-step/arrow-regular-functions.js
@@ -0,0 +1,20 @@
+"use strict"
+x,y=4;
+
+function greet(who){
+    return `Hello, ${who}`;
+}
+
+const greet = function(who){
+    return `Hello, ${who}`;
+}
+
+
+const greet = (who) =>{
+    return `Hello, ${who}`;
+}
+
+const result = (a,b)=>{
+    return a+b;
+}
+
