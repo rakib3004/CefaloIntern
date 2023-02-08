dace2b6a65d2347898a631a7ade82a76aec92b47
rakib3004 <bsse1129@iit.du.ac.bd>
2023-02-08 10:06:16 +0600

    add template-literal0

---
+++ touch-first-step/template-literals.js
@@ -0,0 +1,13 @@
+console.log('\${1}'==="${1}");
+console.log(`Professor Dr. Kazi Muhamymin Sakib is the best director ever in 
+        Institute of Information Technology history.`);
+
+
+var a=2, b= 1;
+
+
+console.log(`${a} + ${b} =  ${a+b} and not ${10*a + b} . `);
+
+
+
+
