1bbfd2baf4b555a0710da497517ec82ed57d91b1
rakib3004 <bsse1129@iit.du.ac.bd>
2023-02-08 10:56:39 +0600

    code about template literal

--- touch-first-step/template-literals.js
+++ touch-first-step/template-literals.js
@@ -1,12 +1,25 @@
-console.log('\${1}'==="${1}");
+console.log("${1}" === "${1}");
 console.log(`Professor Dr. Kazi Muhamymin Sakib is the best director ever in 
         Institute of Information Technology history.`);
 
+var a = 2,
+  b = 1;
 
-var a=2, b= 1;
+console.log(`${a} + ${b} =  ${a + b} and not ${10 * a + b} . `);
+
+let app = `It's my life
+        my rules`;
 
 
-console.log(`${a} + ${b} =  ${a+b} and not ${10*a + b} . `);
+
+let country ='Bangladesh';
+let a1=17;
+let b1=1;
+
+
+let sentence = `I love ${country} and it's population is ${a1+b1} crore.`;
+
+console.log(sentence)
 
 
 
