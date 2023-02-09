e373f355e1c3865c72faf9aac57390712c8f9214
rakib3004 <bsse1129@iit.du.ac.bd>
2023-02-09 16:56:17 +0600

    explore array

---
+++ touch-first-step/javascript-arrays.js
@@ -0,0 +1,19 @@
+console.log(1/0);
+
+const cars = [];
+cars[0]= "One";
+cars[1]= "Two";
+cars[2]= "Three";
+cars[3]= "Four";
+cars[4]= "Five";
+cars[5]= "Six";
+cars[6]= "Seven";
+cars[7]= "Eight";
+cars[8]= "Nine";
+cars[9]= "Ten";
+cars[10]= "Eleven";
+cars[11]= "Twelve";
+cars[12]= "Thirteen";
+cars[13]= "Fourteen";
+cars[14]= "Fifteen";
+console.log(cars)
