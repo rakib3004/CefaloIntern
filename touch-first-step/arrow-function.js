dcce312fccd5f5b3c3c36f2759ee57626477b617
rakib3004 <bsse1129@iit.du.ac.bd>
2023-02-09 11:04:26 +0600

    creat let-const and arrow-function.js file

---
+++ touch-first-step/arrow-function.js
@@ -0,0 +1,21 @@
+function getCertificateName(courseName,courseProviderName){
+
+    return `${courseName}-${courseProviderName}`;
+}
+
+
+
+console.log(getCertificateName('intro-github','coursera'));
+
+
+
+var z = function multiply(x,y){
+    return x*y;
+}
+
+
+var z = (x,y) =>{
+    return x*y;
+}
+
+
---
+++ touch-first-step/let-const.js
@@ -0,0 +1,70 @@
+// Let vs Const
+
+
+var totalCertification=157;
+
+{
+
+
+    {
+
+{
+
+{
+
+{
+
+{
+
+{
+
+{
+
+let totalCertificateInThisYear=18;
+
+    console.log(totalCertificateInThisYear);
+}
+
+
+}
+
+
+}
+
+}
+
+
+
+}
+
+}
+
+    }
+
+}
+
+
+totalCertification++;
+
+console.log(totalCertification);
+
+
+{
+
+{
+    const targetNumberOfCertificateBefore2024=400;
+
+
+{
+    const targetNumberOfCertificateBefore2024=300;
+
+    console.log(targetNumberOfCertificateBefore2024);
+}
+
+console.log(targetNumberOfCertificateBefore2024);
+
+
+}
+
+}
+
