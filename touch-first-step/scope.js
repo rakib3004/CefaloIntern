f4e224f3128776c84d9fab983af51530af92db05
rakib3004 <bsse1129@iit.du.ac.bd>
2023-02-09 09:32:32 +0600

    declare variables in block and global scope

---
+++ touch-first-step/scope.js
@@ -0,0 +1,33 @@
+// global scope
+
+let expectedCGPA = 3.85;
+let currentCGPA = 3.65;
+const highestCGPA = 4.00;
+
+
+// block scope
+
+{
+
+let marksOnDockerProject = 65;
+
+}
+
+
+{
+
+    var totalNumberOfStudentAbove55InDS=17;
+
+}
+
+
+
+{
+
+const totalMarksInDistributedSystemLab=70;
+
+}
+
+
+
+function get
