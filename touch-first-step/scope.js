73de4684ed1a66214af4c3f1cff557e509d93013
rakib3004 <bsse1129@iit.du.ac.bd>
2023-02-09 09:48:37 +0600

    test var const and let in each types of block ['global', 'function', 'block']

--- touch-first-step/scope.js
+++ touch-first-step/scope.js
@@ -4,6 +4,11 @@ let expectedCGPA = 3.85;
 let currentCGPA = 3.65;
 const highestCGPA = 4.00;
 
+console.log(expectedCGPA);
+console.log(currentCGPA);
+console.log(highestCGPA);
+
+
 
 // block scope
 
@@ -13,12 +18,24 @@ let marksOnDockerProject = 65;
 
 }
 
+//console.log(marksOnDockerProject);
+// not allowed let outside of the block scope
+
+
+
+
 
 {
 
     var totalNumberOfStudentAbove55InDS=17;
 
 }
+
+
+//console.log(totalNumberOfStudentAbove55InDS);
+// not allowed var outside of the block scope
+
+
 
 
 
@@ -28,6 +45,41 @@ const totalMarksInDistributedSystemLab=70;
 
 }
 
+//console.log(totalMarksInDistributedSystemLab);
+// not allowed const outside of the block scope
 
 
-function get
+
+
+// function scope
+
+function primaryFunction(){
+ var primary = "department.primary";
+}
+
+//console.log(primary);
+// not allowed var outside of the function scope
+
+
+
+function secondaryFunction(){
+    const secondary = "department.secondary";
+
+}
+
+//console.log(secondary);
+// not allowed const outside of the function scope
+
+
+
+
+function trinaryFunction(){
+    let trinary = "department.trinary";
+}
+
+//console.log(trinary);
+// not allowed let outside of the function scope
+
+
+
+
