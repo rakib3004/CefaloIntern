178d907fb174d864aff9ff78a86b6140ac7dc8f2
rakib3004 <bsse1129@iit.du.ac.bd>
2023-02-09 15:08:58 +0600

    of loops in there

---
+++ touch-first-step/of-loop.js
@@ -0,0 +1,43 @@
+const semester6Courses = [
+  "distributed-systems",
+  "artificial-intelligence",
+  "software-testing",
+  "software-design",
+  "software-metrics",
+  "software-security",
+];
+
+const semester5Courses = [
+  "design-patterns",
+  "web-technology",
+  "database-management-systems-II",
+  "business-communitactions",
+  "professional-ethics",
+  "software-project-lab-II",
+];
+
+
+console.log(`2nd Subject: ${semester6Courses[2]}`);
+
+
+console.log(semester6Courses);
+
+console.log(semester6Courses);
+
+for (course of semester5Courses) {
+  console.log(course);
+}
+
+
+for (course of semester6Courses) {
+    console.log(course);
+  }
+
+
+let companyName = "Cefalo Bangladesh Ltd.";
+
+for(c_name of companyName){
+    console.log(c_name);
+}
+
+
