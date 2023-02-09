a6f04a97d314bbe26e0236a5522914c5ca4a99e3
rakib3004 <bsse1129@iit.du.ac.bd>
2023-02-09 14:52:48 +0600

    spread operators

---
+++ touch-first-step/spread-operator.js
@@ -0,0 +1,13 @@
+let ratings = [1452,3145,862,914,1541,815,923,1243,2012];
+
+let bestRating = Math.max(...ratings)
+
+
+let costs = [140,120,70,80,55,75,180,360,50]
+
+let lowestCost = Math.min(...costs)
+
+console.log(bestRating)
+console.log(lowestCost)
+
+
