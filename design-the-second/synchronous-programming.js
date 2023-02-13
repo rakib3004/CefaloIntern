ea09d9464bff0eb22e8fae9ce6065242cb5e85e6
rakib3004 <bsse1129@iit.du.ac.bd>
2023-02-13 15:17:25 +0600

    start: design the second

---
+++ design-the-second/synchronous-programming.js
@@ -0,0 +1,12 @@
+function createMessage(userID){
+
+return `Welcome to the system, ${userID}`;
+
+}
+
+const user = "iit.du.ac.bd@root"
+const welcomeMessage = createMessage(user)
+console.log(welcomeMessage)
+
+
+
---
+++ reloading-the-first/scopes.js
@@ -0,0 +1,10 @@
+var num1 = 2;
+var num2 = 5;
+
+var sum  = function(){
+    return num1 + num2;
+};
+
+console.dir(sum);
+
+console.log(`<h1>Closure<h1/>`)
