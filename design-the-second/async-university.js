50619849f91c20e12b18737998f8847245df3c37
rakib3004 <bsse1129@iit.du.ac.bd>
2023-02-13 18:50:17 +0600

    async with sync

--- design-the-second/async-university.js
+++ design-the-second/async-university.js
@@ -1,8 +1,31 @@
+
+
+
+setTimeout(function(){
+    console.log("Go to Janata Bank")
+},2000);
+
+
+
+setTimeout(function(){
+    console.log("Go to Hall Office")
+},4000);
+
+
+setTimeout(function(){
+    console.log("Go to Director Room")
+},5000);
+
+
+setTimeout(function(){
+    console.log("Go to Register Building")
+},7000);
+
 console.log("Application for admit card verification");
 
 setTimeout(function(){
     console.log("Admit card is verified")
-},5000);
+},2000);
 
 console.log("Back to Home");
 
