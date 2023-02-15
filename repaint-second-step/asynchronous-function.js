cef065d8b57b24e88a9351aa26ec16192e2e75e1
rakib3004 <bsse1129@iit.du.ac.bd>
2023-02-15 14:31:57 +0600

    take process and complete order

---
+++ repaint-second-step/asynchronous-function.js
@@ -0,0 +1,26 @@
+const takeOrder = (customer, callback) =>{
+    console.log(`Take order for ${customer}`);
+    callback(customer)
+}
+
+const processOrder = (customer, callback) => {
+    console.log(`Processing order for ${customer}`);
+
+
+    setTimeout(()=>{
+        console.log('Cooking complete');
+        console.log(`Order processed for ${customer}`);
+        callback(customer)
+    }, 3000);
+};
+
+
+const completeOrder = (customer) =>{
+    console.log(`Complete order for ${customer}`);
+}
+
+takeOrder("Customer 1", (customer)=>{
+    processOrder(customer, (customer)=>{
+        completeOrder(customer);
+    })
+})
