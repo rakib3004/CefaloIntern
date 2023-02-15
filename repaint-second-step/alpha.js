04e538b2d94202dd409c146c6bb5f306fad1c8f5
rakib3004 <bsse1129@iit.du.ac.bd>
2023-02-15 12:34:24 +0600

    complte alpha.js

---
+++ repaint-second-step/alpha.js
@@ -0,0 +1,13 @@
+const processOrder = (customer) =>{
+    console.log(`Processing order for customer 1`)
+    var currentTime = new Date().getTime();
+while(currentTime+3000>=new Date().getTime());
+console.log('Order process for customer 1');
+}
+
+console.log(`Take order for customer 1`);
+
+processOrder();
+
+console.log(`Complete order for customer 1`);
+
