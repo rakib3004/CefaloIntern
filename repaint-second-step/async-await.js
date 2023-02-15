8023df685831d857fa792ec835d785cdae6cf620
rakib3004 <bsse1129@iit.du.ac.bd>
2023-02-15 12:06:27 +0600

    review async functions

--- repaint-second-step/async-await.js
+++ repaint-second-step/async-await.js
@@ -4,6 +4,7 @@ const a = await getData();
 const b = await getMoreData(a);
 const c = await getMoreData(c);
 const d = await getMoreData(d);
-const e = 0;
+const e = await getMoreData(e);
+console.log(e);
 
 })();
